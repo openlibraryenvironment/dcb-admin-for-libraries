99625ef70f323332dc4ea876704d1d5aa9ac59ff
Ian Ibbotson <ianibbo@gmail.com>
2025-06-13 20:40:48 +0100

    chore: Make basedir relative so that the app can be deployed into a subdirectoru

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-		react(),
-	],
-	build: {
-		// think about other ways of addressing bundle size
-		// ultimately if we can't because of the DGrid it's fine
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					vendor: ["react", "react-dom"],
-					ui: ["@mui/material", "@mui/icons-material"],
-				},
-			},
-		},
-	},
+export default defineConfig(({mode}) => {
+	// Ian: this is done to allow us to deploy the app to a folder rather than the root of a URI.
+  // This approach shold also work for deployment at root.
+  // const env = loadEnv(mode, process.cwd(), '');
+  const bp = './';
+
+	return {
+    plugins: [
+  		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+	  	react(),
+  	],
+    base: bp,
+	  build: {
+  		// think about other ways of addressing bundle size
+	  	// ultimately if we can't because of the DGrid it's fine
+		  rollupOptions: {
+  			output: {
+	  			manualChunks: {
+		  			vendor: ["react", "react-dom"],
+			  		ui: ["@mui/material", "@mui/icons-material"],
+  				},
+	  		},
+		  },
+    },
+	};
 });
