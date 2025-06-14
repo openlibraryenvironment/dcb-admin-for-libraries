3efa3233d3a7c3d1af84db8cd07d51431b66f70d
Ian Ibbotson <ianibbo@gmail.com>
2025-06-14 13:21:46 +0100

    Integrate external config

--- src/main.tsx
+++ src/main.tsx
@@ -15,58 +15,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-LicenseInfo.setLicenseKey(String(import.meta.env.VITE_MUI_X_LICENSE_KEY));
 
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			staleTime: 1000 * 60 * 5, // 5 minutes
-			retry: 1,
-		},
-	},
-});
 
-const getBasePath = () => {
-  const fullPath = window.location.pathname;
-  const matches = fullPath.match(/^\/[^/]+/);
-  return matches ? matches[0] : '/';
-};
-
-// basename is set this way so we can deploy this app to multiple folders and the app will
-// work relative to those folders
-const router = createRouter({
-	routeTree,
-  basepath: getBasePath(),
-	defaultPreload: "intent",
-	defaultPreloadStaleTime: 0,
-	defaultStaleTime: 5000,
-	scrollRestoration: true,
-	context: {
-		auth: undefined!,
-		queryClient,
-	},
-});
-
-// Register things for typesafety
-declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
-}
-
-const oidcConfig = {
-	authority: import.meta.env.VITE_KEYCLOAK_URL,
-	client_id: import.meta.env.VITE_KEYCLOAK_ID,
-	redirect_uri: window.location.origin,
-	response_type: "code",
-	scope: "openid profile email",
-	loadUserInfo: true,
-	automaticSilentRenew: true,
-	onSigninCallback: (_user: User | void): void => {
-		// Remove the query parameters from the URL
-		window.history.replaceState({}, document.title, window.location.pathname);
-	},
-};
 
 function App() {
 	const auth = useAuth();
@@ -81,10 +31,108 @@ function App() {
 	);
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<AuthProvider {...oidcConfig}>
-			<App />
-		</AuthProvider>
-	</React.StrictMode>
-);
+async function getCfg() {
+  try {
+
+    // We need to support runtime configuration as well as build time config. When using
+    // build time config variables come via import.meta.env.VITE_MUI_X_LICENSE_KEY
+    // When using deploy time, cloudflare will inject inject_env.js into the deployment 
+    // to read these values.
+    const response = await fetch('/inject_env.json', { cache: 'no-store' });
+
+		// File not supplied, OR, server helpfully returning the SPA bundle root doc
+    if (!response.ok || response.headers.get("Content-Type")?.includes("text/html")) {
+      return {
+        "VITE_MUI_X_LICENSE_KEY" : String(import.meta.env.VITE_MUI_X_LICENSE_KEY),
+        "VITE_KEYCLOAK_URL": String(import.meta.env.VITE_KEYCLOAK_URL),
+        "VITE_KEYCLOAK_ID" : String(import.meta.env.VITE_KEYCLOAK_ID),
+        "VITE_DCB_API_BASE": String(import.meta.env.VITE_DCB_API_BASE),
+        "VITE_DCB_SEARCH_BASE": String(import.meta.env.VITE_DCB_SEARCH_BASE)
+      };
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.warn('Could not load inject_env.json:', err);
+    return {};
+  }
+}
+
+const getBasePath = () => {
+  const fullPath = window.location.pathname;
+  const matches = fullPath.match(/^\/[^/]+/);
+  return matches ? matches[0] : '/';
+};
+  
+const queryClient = new QueryClient({
+	defaultOptions: {
+	 	queries: {
+			staleTime: 1000 * 60 * 5, // 5 minutes
+			retry: 1,
+	 	},
+ 	},
+});
+
+// basename is set this way so we can deploy this app to multiple folders and the app will
+// work relative to those folders
+const router = createRouter({
+  routeTree,
+  basepath: getBasePath(),
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  defaultStaleTime: 5000,
+  scrollRestoration: true,
+  context: {
+    auth: undefined!,
+    queryClient,
+  },
+});
+
+// Register things for typesafety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  } 
+}
+
+
+
+async function bootstrap() {
+
+  // See if there is an injected_cfg.json, otherwise fall back to build time variables.
+	const cfg = await getCfg();
+
+  // Nice feature of tanstack etc - inject our config bundle into the router context
+  router.update({
+    context: { cfg },
+  });
+
+  LicenseInfo.setLicenseKey(cfg.VITE_MUI_X_LICENSE_KEY);
+
+  const oidcConfig = {
+  	authority: import.meta.env.VITE_KEYCLOAK_URL,
+  	client_id: import.meta.env.VITE_KEYCLOAK_ID,
+  	redirect_uri: window.location.origin,
+  	response_type: "code",
+  	scope: "openid profile email",
+  	loadUserInfo: true,
+  	automaticSilentRenew: true,
+  	onSigninCallback: (_user: User | void): void => {
+  		// Remove the query parameters from the URL
+  		window.history.replaceState({}, document.title, window.location.pathname);
+  	},
+  };
+  
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+  	<React.StrictMode>
+  		<AuthProvider {...oidcConfig}>
+  			<App />
+  		</AuthProvider>
+  	</React.StrictMode>
+  );
+}
+
+// Some reworking here to allow the app to load config from /injected_env.json before
+// proceeding to booting the rest of the SPA. This allows runtime deployment to cloudflare,
+// and for different domain names (clouflare workers) to inject different config blocks.
+bootstrap();
