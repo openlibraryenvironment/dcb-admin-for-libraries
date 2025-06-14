8c1edd3996fe724dd04cc9c4aedc0082cec649db
Ian Ibbotson <ianibbo@gmail.com>
2025-06-14 22:26:48 +0100

    Work on title display

--- src/routes/sharedIndex/$id/index.tsx
+++ src/routes/sharedIndex/$id/index.tsx
@@ -1,9 +1,25 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
+import Grid from "@mui/material/Grid2";
+import Typography from "@mui/material/Typography";
 
 export const Route = createFileRoute('/sharedIndex/$id/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/sharedIndex/$id"!</div>
+
+  const { cfg } = useRouter().options.context as { cfg: any};
+  const { t } = useTranslation();
+
+  return (
+     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid size={{ xs: 4, sm: 8, md: 12 }}>
+        <Typography variant="accordionSummary">
+          {t("nav.library.service")}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+
 }
