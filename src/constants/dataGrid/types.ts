4a3b7ea7e6657c32eba99c026c8e3332123bd61e
Casey Henderson <casey.henderson@k-int.co.uk>
2025-09-05 17:26:52 +0100

    Temporary disable location click through

--- src/constants/dataGrid/types.ts
+++ src/constants/dataGrid/types.ts
@@ -33,7 +33,7 @@ export const presetTypes = [
 
 // Types of data grid where users cannot click through to a details page
 export const nonClickableTypes = [
-	"locations", // TEMPORARY
+	"locations", // VERY TEMPORARY
 	"referenceValueMappings",
 	"ReferenceValueMappings",
 	"numericRangeMappings",
